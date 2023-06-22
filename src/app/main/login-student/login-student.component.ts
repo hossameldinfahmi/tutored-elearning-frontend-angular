41709316d93a6bcbfdbf54c4e08adb7d69e61b21
hossameldinfahmi <hossameldinfahmi@gmail.com>
2023-06-22 23:38:36 +0300

    Create Student's Login Component UI

---
+++ src/app/main/login-student/login-student.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-student',
+  templateUrl: './login-student.component.html',
+  styleUrls: ['./login-student.component.css']
+})
+export class LoginStudentComponent implements OnInit {
+
+  constructor( 
+    ) {
+
+    }
+
+  ngOnInit(): void {
+  }
+
+
+  data={email:"",password:""}
+
+  login(form: NgForm){
+    if(form.invalid){
+      return;
+    }
+
+    this.data.email=form.value.email
+    this.data.password=form.value.password
+
+  
+  }
+
+
+
+}
